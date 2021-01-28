f9b5b520d369752b951470604078384ea394102f
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-01-28 16:26:34 +0900

    EditItem

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new]
+  before_action :set_item, only:[:edit, :update]
 
   def new
   	@item = Item.new
@@ -16,16 +17,28 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    render layout: 'no_menu' # レイアウトファイル指定
+    render layout: 'no_menu'
+  end
+
+  def update
+  	if @item.update(item_params)
+  		redirect_to root_path, notice: "商品の編集が完了しました。"
+  	else
+  		render layout: 'no_menu', action: :edit
+  	end
   end
 
   def purchase_confirmation
-    render layout: 'no_menu' # レイアウトファイル指定
+    render layout: 'no_menu'
   end
 
 
   private
   def item_params
   	params.require(:item).permit(:name, :detail, :category_id, :condition, :delivery_fee_payer, :delivery_method, :prefecture_id, :delivery_days, :price).merge(seller_id: current_user.id)
+  end
+
+  def set_item
+  	@item = Item.find(params[:id])
   end
 end
