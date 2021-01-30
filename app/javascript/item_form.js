6d2e6b3cde92b7ebbd15eaa843a8f75c3efa26bb
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-01-30 15:40:56 +0900

    Change_image

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -5,6 +5,13 @@ document.addEventListener('turbolinks:load', function () {
   $("#select-image-button").on("click", function(){
   	const file_field = $("#item_images_attributes_0_src"); // 新規画像投稿用のfile_fieldを取得する。
     file_field.trigger("click"); // file_fieldをクリックさせる。
+
+  });
+
+  // //file_fieldが変化したら
+  $("#image-file-fields").on("change", `input[type="file"]`, function () { //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
+    // -----変更ここまで-----
+    console.log("画像が選択されました")
   });
   
 });
