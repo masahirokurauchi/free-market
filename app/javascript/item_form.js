0cad5ca64d96ae4248b52a630a98ef00b7a1f644
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-02 16:28:38 +0900

    hide_url_and_file_field

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -3,7 +3,7 @@ document.addEventListener('turbolinks:load', function () {
 
   function newFileField(index) { //新規画像投稿用のfile_fieldを作成しappendする。
   	const html = `
-               <input accept="image/*" class="new-item-image" style="display: block;" data-index="${index}" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
+               <input accept="image/*" class="new-item-image" data-index="${index}" type="file" name="item[images_attributes][${index}][src]" id="item_images_attributes_${index}_src">
                `;
     return html;
   }
