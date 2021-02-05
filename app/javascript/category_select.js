1ffa587ee4cdf36a4f87c5c6965d5c06a260c0a4
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-05 16:50:47 +0900

    create_child_category_form

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -1,6 +1,23 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('.select-category')[0]) return false; //カテゴリのフォームが無いなら以降実行しない。
 
+  function buildCategoryForm(categories) { // 子孫カテゴリのフォームを組み立てる
+    let options = "";
+    categories.forEach(function (category) { // カテゴリを一つずつ渡してoptionタグを一つずつ組み立てていく。
+      options += `
+                  <option value="${category.id}">${category.name}</option>
+                 `;
+    });
+
+    const html = `
+                  <select required="required" class="select-category" name="item[category_id]">
+                    <option value="">---</option>
+                    ${options}
+                  </select>
+                 `;
+    return html;
+  }
+
   $(".input-field-main").on("change", ".select-category", function () { //カテゴリが選択された時
     const category_id = $(this).val();
     console.log("選択されたカテゴリのID:", category_id);
@@ -13,6 +30,9 @@ document.addEventListener('turbolinks:load', function () {
         dataType: 'json',
       }).done(function (categories) {
         console.log("success")
+        console.table(categories);
+        const html = buildCategoryForm(categories);
+        console.log(html);
       })
       .fail(function () {
         alert('error');
