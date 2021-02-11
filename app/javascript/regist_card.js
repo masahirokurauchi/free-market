23e8133c87923e2534c0414c665d820c0fa46999
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-11 15:59:13 +0900

    set_trigger_if_button_is_clicked

--- app/javascript/regist_card.js
+++ app/javascript/regist_card.js
@@ -1,4 +1,13 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('#card_form')[0]) return false; //カード登録ページではないなら以降実行しない。
-  console.log("hoge");
+  
+  Payjp.setPublicKey("pk_test_9993b7a16e4609ddc21ad47f"); //公開鍵を読み込む。
+
+  const regist_button = $("#regist_card"); //カード入力フォームの登録ボタン。
+
+  //登録ボタンを押したとき
+  regist_button.on("click", function (e) { //登録ボタンを押したとき（ここはsubmitではなくclickにしておく）。
+      e.preventDefault();
+      console.log(this);    
+    }); 
 });
