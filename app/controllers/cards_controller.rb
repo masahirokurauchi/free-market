f9c0bc394b135749f560fb00ac2b40246369e7a0
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-12 16:18:01 +0900

    set_new_and_create_action

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,11 @@
 class CardsController < ApplicationController
+
+  def new
+    @card = Card.new
+    ## @exp_yearなどの定義がある場合はそのままにしておく
+  end
+
+  def create
+    redirect_to cards_path, alert: "カードの登録が完了しました。トークン：#{params[:payjp_token]}"
+  end
 end
