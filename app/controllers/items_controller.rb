410c5543ff733fcfb40babf4c3bbe684925c947c
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-12 17:13:12 +0900

    set_dealed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -61,11 +61,15 @@ class ItemsController < ApplicationController
   def purchase
   	Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
     customer_token = current_user.card.customer_token
+
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
       customer: customer_token, # 顧客、もしくはカードのトークン
       currency: 'jpy'  # 通貨の種類
     )
+
+    @item.update(deal: "売り切れ")
+    
     redirect_to item_path(@item), notice: "商品を購入しました"
   end
 
