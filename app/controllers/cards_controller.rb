37a00d6203df350903387d731ea5214b07d57f2d
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-12 16:31:30 +0900

    save_card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,6 +6,17 @@ class CardsController < ApplicationController
   end
 
   def create
-    redirect_to cards_path, alert: "カードの登録が完了しました。トークン：#{params[:payjp_token]}"
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key] ## APIキーの取得
+
+    customer = Payjp::Customer.create(card: params[:payjp_token]) ## 顧客の作成
+
+    card = current_user.build_card(card_token: params[:card_token], customer_token: customer.id)
+
+    if card.save
+      redirect_to cards_path, notice: "カードの登録が完了しました。"
+    else
+      redirect_to new_card_path, alert: "カードの登録に失敗しました。"
+    end
   end
+
 end
