5ad8c44afa1b15fd8e71fc3a8070744c4b787810
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-16 16:24:45 +0900

    change_items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def show
-		@selling_items = current_user.selling_items
+		@items = current_user.selling_items
 		@bought_items = current_user.bought_items
 	end
 
