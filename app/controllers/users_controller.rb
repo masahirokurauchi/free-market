be9aa865d9661e5739cf1efc1cbc17430c6faf9b
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-16 16:23:26 +0900

    define_items_by_condition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,25 @@ class UsersController < ApplicationController
 	def show
 		@selling_items = current_user.selling_items
 		@bought_items = current_user.bought_items
+	end
+
+	def selling
+		
+	end
+
+	def selling_progress
+		
+	end
+
+	def sold
+		
+	end
+
+	def bought_progress
+		
+	end
+
+	def bought_past
 		
 	end
 end
