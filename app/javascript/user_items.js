54b0225720efec39e23721b3f80bedc2be121be4
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-16 17:01:54 +0900

    create_jbulider

--- app/javascript/user_items.js
+++ app/javascript/user_items.js
@@ -2,6 +2,26 @@ document.addEventListener('turbolinks:load', function () {
   if (!$('.mypage-block')[0]) return false; //ユーザーマイページではないなら以降実行しない。
   
 
-  
+  $("#selling").on("click", function (e) { //出品中ボタンが押された時
+    e.preventDefault(); //デフォルトで発動するリンクの遷移を防ぐ
+    console.log("selling")
+    
+
+    $.ajax({ //AJAXでuser/sellingを動かす
+        url: "/users/selling",
+        type: "GET",
+        dataType: 'json',
+      }).done(function (items) {
+        if (items.length == 0) return false //categoryが空、つまり孫が選択された場合、処理を終了させる。
+
+        // changed_form.nextAll(".select-category").remove(); //選択肢たカテゴリ以降のカテゴリを全て消去。カテゴリの選び直し対策。
+
+        // const html = buildCategoryForm(categories);// カテゴリのフォームを組み立てる
+        // $(".select-category:last").after(html);// 組み立てたフォームを表示
+      })
+      .fail(function () {
+        alert('error');
+      })
+  });
   
 });
