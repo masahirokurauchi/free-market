2487ad2018fe1efee2d1a0cb55e1748862b82687
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-20 15:47:16 +0900

    build_html

--- app/javascript/user_items.js
+++ app/javascript/user_items.js
@@ -3,6 +3,19 @@ document.addEventListener('turbolinks:load', function () {
 
   function builditems(items, active_id) { // 商品の一覧を組み立てる
     let options = "";
+    let selling_active, selling_progress_active, sold_active, bought_progress_active, bought_past_active = "";
+
+    if (active_id == 1) {
+      selling_active = "active-tab"
+    } else if (active_id == 2) {
+      selling_progress_active = "active-tab"
+    } else if (active_id == 3) {
+      sold_active = "active-tab"
+    } else if (active_id == 4) {
+      bought_progress_active = "active-tab"
+    } else {
+      bought_past_active = "active-tab"
+    }
 
     if (active_id == 1 || active_id == 2 || active_id == 3) { //出品商品の場合
       if (items.length == 0) {
@@ -34,21 +47,8 @@ document.addEventListener('turbolinks:load', function () {
                      `;
         });
       } 
-    } else { //購入商品の場合
 
-    }
-
-    let selling_active, selling_progress_active, sold_active = "";
-
-    if (active_id == 1) {
-      selling_active = "active-tab"
-    } else if (active_id == 2) {
-      selling_progress_active = "active-tab"
-    } else if (active_id == 3) {
-      sold_active = "active-tab"
-    }
-
-    const html = `
+      const html = `
                   <div id ="selling-tabs" class ="mypage-block-tabs">
                     <a id="selling" class="mypage-block-tabs__tab ${selling_active}">
                       出品中
@@ -64,7 +64,61 @@ document.addEventListener('turbolinks:load', function () {
                     ${options}
                   </div>
                  `;
-    return html;
+      return html;
+
+    } else { //購入商品の場合
+      if (items.length == 0) {
+        options = `
+                    <div class ="mypage-block-data-list__data-none">
+                      <div class ="text">
+                        商品はありません。
+                      </div>
+                    </div>
+                   `;
+      } else {
+
+        items.forEach(function (item) { // 商品を一つずつ渡して、一つずつ組み立てていく。
+          options += `
+                      <a href="/items/${item.id}">
+                        <div class ="mypage-block-data-list__data">
+                          <div class ="item-data">
+                            <img src =${item.image} alt ="image" class ="item-data__image">
+                            <div class="item-data__detail">
+                              「
+                              ${item.name}
+                              」
+                              ${item.detail}
+                            </div>
+                            <img src ="/assets/icon-arrow-ffd4a8f478b85be67159e2805ddce1ab337dfabcae57d013392b873f3f06495f.png" class ="item-data__arrow">
+                          </div>
+                        </div>
+                      </a>
+                     `;
+        });
+      }
+
+      const html = `
+                  <div id ="bought-tabs" class ="mypage-block">
+                    <div class ="mypage-block-header">
+                      購入した商品
+                    </div>
+                    <div id ="bought-tabs" class ="mypage-block-tabs">
+                      <a id="bought_progress" class="mypage-block-tabs__tab ${bought_progress_active}">
+                        取引中
+                      </a>
+                      <a id="bought_past" class="mypage-block-tabs__tab ${bought_past_active}">
+                        過去の取引
+                      </a>
+                    </div>
+                    <div id="bought-items-tabs" class ="mypage-block-data-list">
+                      ${options}
+                    </div>
+                  </div>
+                 `;
+      return html;
+
+    }
+
   }
   
 
@@ -121,7 +175,7 @@ document.addEventListener('turbolinks:load', function () {
         } else { //購入商品の場合
           $("#bought-tabs").remove(); //タブと商品の消去
 
-          // const html = builditems(items, active_id);// 商品を組み立てる
+          const html = builditems(items, active_id);// 商品を組み立てる
         }
 
         
