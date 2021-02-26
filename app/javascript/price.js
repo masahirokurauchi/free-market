f357d1899b363bba8074935924bb9d73e3faecb8
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-26 16:52:14 +0900

    set_feeand_profit

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,32 +1,23 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('.select-category')[0]) return false; //商品出品ページでないなら以降実行しない。
 
-  function buildCategoryForm(categories) { // 子孫カテゴリのフォームを組み立てる
-    let options = "";
-    categories.forEach(function (category) { // カテゴリを一つずつ渡してoptionタグを一つずつ組み立てていく。
-      options += `
-                  <option value="${category.id}">${category.name}</option>
-                 `;
-    });
-
-    const html = `
-                  <select required="required" class="select-category" name="item[category_id]">
-                    <option value="">---</option>
-                    ${options}
-                  </select>
-                 `;
-    return html;
-  }
-
 
   $("#item_price").on("keyup", function () { //価格が入力された時
 
+    $("#fee").text();
+    $("#profit").text();
+
     let price = $(this).val();
-    let fee = price / 10;
+    
 
     if ($.isNumeric(price) && (price >= 300) ) { //　数字かつ３００円以上なら
-        console.log(fee);
+        let fee = Math.round(price / 10);
+        let profit = price - fee
+        
+        $("#fee").text(fee);
+        $("#profit").text(profit);
     } else if ($.isNumeric(price)) { // 数字かつ３００円以下なら
+
     } else { // 数字でないなら
 
     }
