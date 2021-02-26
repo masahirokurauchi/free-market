cae56d8b9793222d9f51b949693e2719a9448560
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-26 17:15:31 +0900

    show_error

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -4,25 +4,32 @@ document.addEventListener('turbolinks:load', function () {
 
   $("#item_price").on("keyup", function () { //価格が入力された時
 
-    $("#fee").text(); //以前の手数料を削除
-    $("#profit").text(); //以前の利益を削除
+    $("#fee").text(""); //以前の手数料を削除
+    $("#profit").text(""); //以前の利益を削除
+
+    $("#price-error").text("");
 
     let price = $(this).val(); //価格を取得
     
 
-    if ($.isNumeric(price) && (price >= 300) ) { //　数字かつ３００円以上なら
+    if ($.isNumeric(price)) { //　数字かつ３００円以上なら
+      if ((price >= 300) && (price <= 9999999)) { //　数字かつ３００円以上なら
+        $("#price-error").text();
+
         let fee = Math.round(price / 10); //手数料を計算
         let profit = price - fee //利益を計算
         
         $("#fee").text(fee); //手数料を表示
         $("#profit").text(profit); //利益を表示
-    } else if ($.isNumeric(price)) { // 数字かつ３００円以下なら
+
+      } else { // 数字かつ３００円以下か９９９９９９９円以上なら
+        $("#price-error").text("300~9,999,999円以下にしてください");
+      }
 
     } else { // 数字でないなら
+         $("#price-error").text("数字を入力してください");
 
     }
-
-    
 
     
   });
