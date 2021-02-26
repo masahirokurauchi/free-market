8e93c9d375e03182bf6df8f952ef20aae9456bb8
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-26 16:58:31 +0900

    note comment

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -4,18 +4,18 @@ document.addEventListener('turbolinks:load', function () {
 
   $("#item_price").on("keyup", function () { //価格が入力された時
 
-    $("#fee").text();
-    $("#profit").text();
+    $("#fee").text(); //以前の手数料を削除
+    $("#profit").text(); //以前の利益を削除
 
-    let price = $(this).val();
+    let price = $(this).val(); //価格を取得
     
 
     if ($.isNumeric(price) && (price >= 300) ) { //　数字かつ３００円以上なら
-        let fee = Math.round(price / 10);
-        let profit = price - fee
+        let fee = Math.round(price / 10); //手数料を計算
+        let profit = price - fee //利益を計算
         
-        $("#fee").text(fee);
-        $("#profit").text(profit);
+        $("#fee").text(fee); //手数料を表示
+        $("#profit").text(profit); //利益を表示
     } else if ($.isNumeric(price)) { // 数字かつ３００円以下なら
 
     } else { // 数字でないなら
