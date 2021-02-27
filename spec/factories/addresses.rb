18971cb5bcce537f0760ef4824d516d8f8e47d55
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-02-27 14:25:28 +0900

    create address factroy

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :address do
+    phone_number { Faker::PhoneNumber.cell_phone.gsub(/-/, '') }
+    prefecture_id { rand(1..47) }
+    postal_code { Faker::Address.postcode }
+    city { Faker::Address.city }
+    house_number { Faker::Address.building_number }
+    building_name { Faker::Address.street_name }
+    ## association :userを入れるとuserを作る際に無限ループする
+  end
+end
