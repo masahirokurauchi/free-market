161d54d7c363fea59b3c81761d35274a22b34f2e
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-01 16:25:34 +0900

    set card moc

--- spec/models/payjp_spec.rb
+++ spec/models/payjp_spec.rb
@@ -18,6 +18,31 @@ RSpec.describe Card, type: :model do
         puts card_token.card_token
         puts "ーーーーーーーーーーーーーーーー"
       end  ## /it "card_tokenオブジェクト本来の挙動"
+      
+      ## モックの実装
+      it "モックとallowの挙動確認" do
+        test_mock = double("TestMock") ## doubleで作られるモックオブジェクトは本来の返り値の代用品であり、ただの身代わりのようなもの.
+
+        dummy_card_token = "car_abcdefghijklmnopqrstuvwxyz"
+
+        ## card_tokenオブジェクトに対してcustomer_tokenメソッドを実行した時、代わりにtest_mockを返す
+        allow(card_token).to receive(:customer_token).and_return(test_mock)
+
+        ## card_tokenオブジェクトに対してcard_tokenメソッドを実行した時、代わりにdummy_card_tokenを返す
+        allow(card_token).to receive(:card_token).and_return(dummy_card_token)
+
+        ## test_mockに対してhelloメソッドを実行した時、代わりに"こんにちわ"を返す
+        allow(test_mock).to receive(:hello).and_return("こんにちわ")
+
+        puts "ーーーーーーーーーーーーーーーー"
+        puts "カスタマーのトークン"
+        puts card_token.customer_token
+        puts "\nカードのトークン"
+        puts card_token.card_token
+        puts "\ntest_mockが挨拶をします"
+        puts card_token.customer_token.hello
+        puts "ーーーーーーーーーーーーーーーー"
+      end  ## /it "モックとallowの挙動確認" 
 
     end  ## /context "正しいカード情報が送られている"
 
