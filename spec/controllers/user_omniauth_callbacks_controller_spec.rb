fd93b8c1ed91b0bfe2aecd8f2589437df313ec39
masahirokurauchi <44106989+masahirokurauchi@users.noreply.github.com>
2021-03-01 17:09:13 +0900

    create omniauth test

---
+++ spec/controllers/user_omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Users::OmniauthCallbacksController, type: :controller do
+
+  context 'Google認証のテスト' do
+
+  	let(:dummy_google_user) { OmniAuth::AuthHash.new(DummyData::GoogleUser.data) }
+
+    before do
+    	request.env["devise.mapping"] = Devise.mappings[:user]
+
+    	request.env["omniauth.auth"] = OmniAuth::AuthHash.new(DummyData::GoogleUser.data)
+    end
+
+    it 'Googleで認証後に返ってくるauth情報がダミーデータと一致する' do
+      post 'google_oauth2'
+
+      puts "ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー"
+      puts "session['devise.sns_auth'][:user].email: #{session["devise.sns_auth"][:user].email}"
+      puts "dummy_google_user.info.email: #{dummy_google_user.info.email}"
+      puts "\nsession[:sns_credential].uid: #{session["devise.sns_auth"][:sns_credential].uid}"
+      puts "dummy_google_user.uid: #{dummy_google_user.uid}"
+      puts "ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー"
+
+      expect(session["devise.sns_auth"][:user].email).to eq dummy_google_user.info.email
+      expect(session["devise.sns_auth"][:sns_credential].uid).to eq dummy_google_user.uid
+    end
+
+  end
+
+end
